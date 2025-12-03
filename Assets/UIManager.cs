5b074760812048e13c16af25ba737e66c36f1888
ViviRVCC <G00330203@stu.raritanval.edu>
2025-12-03 12:42:50 -0500

    step3UI2

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -1,11 +1,12 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public Animator startButton;
-public Animator settingsButton;
-
 public class UIManager : MonoBehaviour
 {
+    public Animator startButton;
+    public Animator settingsButton;
+    public Animator dialog;
+
     public void StartGame()
     {
         SceneManager.LoadScene("RocketMouse");
@@ -14,6 +15,13 @@ public class UIManager : MonoBehaviour
     {
         startButton.SetBool("isHidden", true);
         settingsButton.SetBool("isHidden", true);
-    }
 
+        dialog.SetBool("isHidden", false);
+    }
+    public void CloseSettings()
+    {
+        startButton.SetBool("isHidden", false);
+        settingsButton.SetBool("isHidden", false);
+        dialog.SetBool("isHidden", true);
+    }
 }
